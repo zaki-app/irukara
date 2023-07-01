c22e3fd85d0f8f6aa048b1c0ab05e2ee8d4907ae
zaki-dev <matchnokaori@gmail.com>
2023-07-01 22:53:33 +0900

    一覧、詳細API修正

--- src/app/mypage/[id]/page.tsx
+++ src/app/mypage/[id]/page.tsx
@@ -1,32 +1,39 @@
 import React from 'react';
 import Link from 'next/link';
+import { fetchMessageDetail } from '@/common/libs/fetchMessage';
+import type { SaveMessageData } from '@/common/types/LineTypes';
 
-interface Repository {
-  id: number;
-  name: string;
-  full_name: string;
+interface SearchParamsProps {
+  searchParams: {
+    messageId: string;
+  };
 }
 
 export default async function MyPageDetail({
-  params,
-}: {
-  params: { id: string };
-}) {
-  // data fetch
-  const response = await fetch('https://api.github.com/repos/vercel/next.js', {
-    cache: 'no-store',
-  });
-  const data: Repository = await response.json();
+  searchParams,
+}: SearchParamsProps) {
+  const data: SaveMessageData | boolean = await fetchMessageDetail(
+    searchParams.messageId,
+  );
 
   return (
     <div>
-      <h1>マイページです</h1>
-      <p>{params.id}</p>
-      <p>
-        Next.js
-        {data.full_name}
-      </p>
-      <Link href='/'>トップへ戻る</Link>
+      {data ? (
+        <div>
+          <h1>マイページです</h1>
+          <p>
+            Next.js
+            {/* {data.full_name} */}
+          </p>
+          <div>
+            {/* <p>{data.question}</p>
+            <p>{data.answer}</p> */}
+          </div>
+          <Link href='/'>トップへ戻る</Link>
+        </div>
+      ) : (
+        <div>データが見つかりません</div>
+      )}
     </div>
   );
 }
