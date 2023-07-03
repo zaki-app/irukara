d07ff142be2ffc9fec2be196479e2a7a1ca7eb63
zaki-dev <matchnokaori@gmail.com>
2023-07-03 21:58:38 +0900

    現在のURLを取得

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,12 +7,14 @@ import {
   TopLastMessage,
 } from '@/components/client/organisms';
 import { getCookie } from '@/common/utils/authLINE/manageCookies';
+import CurrentUrl from '@/components/client/atoms/CurrentUrl';
 
 export default async function Home() {
   const isOsWeb = (await getCookie('os')) ? await getCookie('os') : 'false';
   console.log('osあるか？', isOsWeb);
   return (
     <div>
+      <CurrentUrl />
       <TopService isWeb={isOsWeb ?? ''} />
       <TopServiceDescription />
       <TopUsedService />
