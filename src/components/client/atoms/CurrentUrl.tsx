501c683aaf2a2e90cdcd8b9cf76fc1800e8437f6
zaki-dev <matchnokaori@gmail.com>
2023-07-03 23:30:11 +0900

    現在のURLを取得

--- src/components/client/atoms/CurrentUrl.tsx
+++ src/components/client/atoms/CurrentUrl.tsx
@@ -7,17 +7,24 @@ import liff from '@line/liff';
 export default function CurrentUrl() {
   const [currentUrl, setCurrentUrl] = useState('');
   const [userOs, setUserOs] = useState('');
+  const [userAgent, setUserAgent] = useState('');
   console.log('現在のURL', window.location.href);
+  console.log(
+    'userAgent これにLIFFだと最後にLIFFが入る',
+    window.navigator.userAgent,
+  );
 
   useEffect(() => {
     setCurrentUrl(window.location.href);
     setUserOs(liff.getOS() ?? '');
+    setUserAgent(window.navigator.userAgent);
   });
 
   return (
     <Suspense fallback='現在のURL読み込み中'>
       <div>現在のURL: {currentUrl}</div>
       <div>{userOs}</div>
+      <div>{userAgent}</div>
     </Suspense>
   );
 }
