29570c74b374ef66dc54fe4162c0fbdc75178c8b
zaki-dev <matchnokaori@gmail.com>
2023-08-15 22:51:08 +0900

    メッセージシェアボタン作成中

--- src/common/types/fetchData.ts
+++ src/common/types/fetchData.ts
@@ -1,4 +1,6 @@
 /* データ関係の型定義 */
+
+// MessagesTable
 export interface SaveMessageData {
   messageId: string;
   answer: string;
@@ -12,6 +14,7 @@ export interface SaveMessageData {
   userId: string;
 }
 
+// ImagesTable
 export interface SaveImageData {
   createdAt: number;
   good: number;
@@ -26,18 +29,23 @@ export interface SaveImageData {
   updatedAt?: number;
 }
 
+// 保存メッセージ取得
 export interface SaveMessageDataType {
   count: number;
   data: SaveMessageData[] | boolean;
 }
 
+// 保存画像データ取得
 export interface SaveImageDataType {
   count: number;
   data: SaveImageData[] | boolean;
 }
 
-// 保存メッセージ
-export interface SaveMessage {
-  count: number;
-  data: SaveMessageData;
+// メッセージ更新プロップス
+export interface UpdateMessageData {
+  messageId: string;
+  memberStatus?: number;
+  mode?: number;
+  referenceType?: number;
+  shareStatus?: number;
 }
