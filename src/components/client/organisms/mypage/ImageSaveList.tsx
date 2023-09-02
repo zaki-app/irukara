79b7bdbaf6512f2bd8770ee1788a67e4abf7864b
zaki-dev <matchnokaori@gmail.com>
2023-09-02 12:20:14 +0900

    logo, img変更

--- src/components/client/organisms/mypage/ImageSaveList.tsx
+++ src/components/client/organisms/mypage/ImageSaveList.tsx
@@ -13,26 +13,26 @@ interface ImageDataProps {
 // 遅延
 // await new Promise((resolve) => setTimeout(resolve, 1000))
 
-async function getSaveImages(mode: number) {
-  const endpoint = `/api/image?mode=${mode}`;
-  const res = await fetch(endpoint, {
-    method: 'GET',
-    cache: 'force-cache',
-  });
-  const { imageData } = await res.json();
+// async function getSaveImages(mode: number) {
+//   const endpoint = `/api/image?mode=${mode}`;
+//   const res = await fetch(endpoint, {
+//     method: 'GET',
+//     cache: 'force-cache',
+//   });
+//   const { imageData } = await res.json();
 
-  return imageData;
-}
+//   return imageData;
+// }
 
-async function fetchSaveImage(page: number, mode: number) {
-  const { data } = await getSaveImages(mode);
-  return data.slice((page - 1) * 2, page * 2);
-}
+// async function fetchSaveImage(page: number, mode: number) {
+//   const { data } = await getSaveImages(mode);
+//   return data.slice((page - 1) * 2, page * 2);
+// }
 
 export default function ImageSaveList({ mode }: ImageDataProps) {
   console.log('image save props', mode);
-  const [imageData, setImageData] = useState<SaveImageData>();
-  const [imageCount, setImageCount] = useState<number>(0);
+  // const [imageData, setImageData] = useState<SaveImageData>();
+  // const [imageCount, setImageCount] = useState<number>(0);
 
   useMemo(async () => {
     // const endpoint = `/api/image?mode=${mode}`;
@@ -45,27 +45,27 @@ export default function ImageSaveList({ mode }: ImageDataProps) {
     // setImageData(imageData.data);
   }, []);
 
-  const { data, fetchNextPage, isFetchingNextPage } = useInfiniteQuery(
-    ['query'],
-    async ({ pageParam = 1 }) => {
-      const response = await fetchSaveImage(pageParam);
-      return response;
-    },
-    {
-      getNextPageParam: (foo, pages) => {
-        console.log('何が入ってる？', foo);
-        console.log('ページ', pages);
-        return pages.length + 1;
-      },
-      initialData: {
-        pages: [],
-      },
-    },
-  );
+  // const { data, fetchNextPage, isFetchingNextPage } = useInfiniteQuery(
+  //   ['query'],
+  //   async ({ pageParam = 1 }) => {
+  //     const response = await fetchSaveImage(pageParam);
+  //     return response;
+  //   },
+  //   {
+  //     getNextPageParam: (foo, pages) => {
+  //       console.log('何が入ってる？', foo);
+  //       console.log('ページ', pages);
+  //       return pages.length + 1;
+  //     },
+  //     initialData: {
+  //       pages: [],
+  //     },
+  //   },
+  // );
 
   return (
     <div>
-      {imageCount > 0 && Array.isArray(imageData) ? (
+      {/* {imageCount > 0 && Array.isArray(imageData) ? (
         imageData.map((item) => (
           <div key={item.imageId}>
             <Suspense fallback='イラスト画像関係をローディング'>
@@ -83,7 +83,8 @@ export default function ImageSaveList({ mode }: ImageDataProps) {
         <div>
           {mode === 1 ? 'イラストデータ' : 'リアル画像データ'}が見つかりません
         </div>
-      )}
+      )} */}
+      修正中
     </div>
   );
 }
