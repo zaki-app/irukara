9746f68bff7fa90c2ca0b6a6567776feb3c0041f
zaki-dev <matchnokaori@gmail.com>
2023-10-02 22:00:56 +0900

    サインアウト修正中

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,5 @@
 /* 全体で使用するnode.jsミドルウェア */
 export {} from 'next-auth/middleware';
 
-export const config = { matcher: ['/mypage', '/mypage/[id]'] };
+// export const config = { matcher: ['/mypage', '/mypage/[id]'] };
+export default function config() {}
