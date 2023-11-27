5e8ec7f4b79d25b3f89dbf8f4fd012e73c392c9e
zaki-dev <matchnokaori@gmail.com>
2023-11-27 09:30:52 +0900

    cookie修正

--- src/common/utils/manageCookies.ts
+++ src/common/utils/manageCookies.ts
@@ -1,6 +1,13 @@
 'use server';
 
 import { cookies } from 'next/headers';
+import { COOKIE_NAME } from '../constants';
+
+// cookieから値を取得する
+export async function getCookie(name: string) {
+  const cookie = cookies().get(name);
+  return cookie?.value;
+}
 
 // cookiesに保存する
 export async function setCookie(name: string, value: string, option?: Date) {
@@ -10,88 +17,44 @@ export async function setCookie(name: string, value: string, option?: Date) {
 
   const expires = option || defaultExpires;
 
+  console.log('セットクッキー', expires);
+
   cookies().set({
     name,
     value,
     httpOnly: true,
     path: '/',
+    expires,
+    secure: true,
   });
-  cookies().set(name, value, { expires });
   console.timeEnd('cookies');
-}
-
-// cookiesを削除
-export async function deleteCookie() {
-  console.time('cookies2');
-  cookies().set({
-    name: 'auth',
-    value: '',
-    expires: new Date('2016-10-6'),
-    path: '/',
-  });
-  cookies().set({
-    name: 'irukaraAT',
-    value: '',
-    expires: new Date('2016-10-6'),
-    path: '/',
-  });
-  cookies().set({
-    name: 'irukaraId',
-    value: '',
-    expires: new Date('2016-10-6'),
-    path: '/',
-  });
-  cookies().set({
-    name: 'browser',
-    value: '',
-    expires: new Date('2016-10-6'),
-    path: '/',
-  });
-  console.timeEnd('cookies2');
-}
-
-/** cookieにアクセストークンとuserIdがあるかの存在確認 */
-export async function isCookie() {
-  const accessToken = cookies().get('irukaraAT');
-  const userId = cookies().get('irukaraId');
-  let isCookieValue;
-  if (accessToken || !userId) {
-    console.log('クッキーなし');
-    isCookieValue = false;
-  } else {
-    console.log('クッキーあり');
-    isCookieValue = true;
-  }
-
-  return isCookieValue;
-}
-
-// cookieから値を取得する
-export async function getCookie(name: string) {
-  const cookie = cookies().get(name);
-  return cookie?.value;
 }
 
 // cookieを全て削除
 export async function allDeleteCookies() {
   console.time('cookies2');
   cookies().set({
-    name: 'provider',
+    name: COOKIE_NAME.IRUKARA_ID,
     value: '',
     expires: new Date('2016-10-6'),
     path: '/',
   });
   cookies().set({
-    name: 'irukaraAT',
+    name: COOKIE_NAME.IRUKARA_JWT,
     value: '',
     expires: new Date('2016-10-6'),
     path: '/',
   });
   cookies().set({
-    name: 'irukaraID',
+    name: COOKIE_NAME.IRUKARA_PROVIDER,
     value: '',
     expires: new Date('2016-10-6'),
     path: '/',
   });
   console.timeEnd('cookies2');
+}
+
+// 特定のcookieを削除
+export async function deleteCookie(name: string) {
+  cookies().delete(name);
 }
