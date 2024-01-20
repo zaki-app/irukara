58702bc18745ce359db141fd87d4332c447ab0ba
zaki-dev <matchnokaori@gmail.com>
2024-01-20 14:28:05 +0900

    リアル画像表示修正

--- src/components/client/molecules/login/chatgpt/ChatGpt.tsx
+++ src/components/client/molecules/login/chatgpt/ChatGpt.tsx
@@ -1,10 +1,7 @@
 'use client';
 
-// import { COOKIE_NAME } from '@/common/constants';
-// import { API } from '@/common/constants/path';
+import { DATA, SELECT_MODE } from '@/common/constants';
 import { currentUnix } from '@/common/libs/dateFormat';
-// import { getCookie } from '@/common/utils/cookie/manageCookies';
-// import InputPrompt from '@/components/client/atoms/login/InputPrompt';
 import AiCard from '@/components/client/atoms/login/chat/AiCard';
 import UserCard from '@/components/client/atoms/login/chat/UserCard';
 import ScrollBottom from '@/components/client/atoms/scroll/ScrollBottom';
@@ -13,13 +10,17 @@ import { setSpinner } from '@/store/ui/spinner/slice';
 import { HistoryDataMessageRes, MessageType } from '@/types/message';
 import { Message } from 'ai/react';
 import { Spin } from 'antd';
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { useSelector } from 'react-redux';
 
 /**
  * GenerateAreaで生成されたChatGptのやりとりを表示する
- * @param  ユーザーとIrukaraのやり取り
+ *
+ * @param todayData サーバー側で取得した今日のデータ一覧配列
+ * @param messages 今日の新しく生成されたメッセージ
  * @param type 1が今日、2が7日間
+ * @param newMessage 保存されたメッセージオブジェクト
+ * @param historyData 過去の指定されたメッセージ配列
  * @returns
  */
 export default function ChatGpt({
@@ -44,14 +45,15 @@ export default function ChatGpt({
   const [isLoaded, setLoaded] = useState<boolean>(false);
 
   useEffect(() => {
-    if (selectedMenu === 0 && type === 1) {
+    if (selectedMenu === SELECT_MODE.GPT3 && type === DATA.TODAY) {
+      // 今日のデータ
       store.dispatch(setSpinner({ isSpinner: true }));
       setDataMessages(todayData as MessageType[]);
       setDataCount(todayData?.length as number);
       setLoaded(true);
       store.dispatch(setSpinner({ isSpinner: false }));
-      // })();
-    } else if (type === 2 && historyData) {
+    } else if (type === DATA.HISTORY && historyData) {
+      // 履歴のデータ
       store.dispatch(setSpinner({ isSpinner: true }));
       setDataMessages(historyData?.data);
       setDataCount(historyData?.count);
@@ -65,19 +67,23 @@ export default function ChatGpt({
       {isLoaded ? (
         <ScrollBottom className='relative w-full h-full overflow-y-auto px-2 md:px-4'>
           <div className='flex flex-col-reverse'>
-            {dataMessages?.map((data) => (
-              <div key={data.messageId} className='test'>
-                {/* ユーザー */}
-                <UserCard question={data.question} createdAt={data.createdAt} />
-                {/* Irukara */}
-                <AiCard
-                  answer={data.answer}
-                  createdAt={data.createdAt}
-                  messageId={data.messageId}
-                  shareStatus={data.shareStatus}
-                />
-              </div>
-            ))}
+            {dataMessages &&
+              dataMessages?.map((data) => (
+                <div key={data.messageId} className='test'>
+                  {/* ユーザー */}
+                  <UserCard
+                    question={data.question}
+                    createdAt={data.createdAt}
+                  />
+                  {/* Irukara */}
+                  <AiCard
+                    answer={data.answer}
+                    createdAt={data.createdAt}
+                    messageId={data.messageId}
+                    shareStatus={data.shareStatus}
+                  />
+                </div>
+              ))}
           </div>
           {/* 追加のやり取り */}
           {messages &&
@@ -103,7 +109,6 @@ export default function ChatGpt({
             ))}
         </ScrollBottom>
       ) : numDataCount <= 0 ? (
-        // <InputPrompt type={1} />
         <p>今日のデータが見つかりません</p>
       ) : (
         <Spin />
